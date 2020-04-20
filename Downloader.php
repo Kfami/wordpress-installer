2c67cb32410a478b261a0a0897718077fbeabb43
Varun Kumar <varun@Varuns-Mac-mini.local>
2020-04-20 06:18:52 +0530

    Minor Fixes

--- Downloader.php
+++ Downloader.php
@@ -1,62 +1,67 @@
 <?php
+declare(strict_types=1);
 
 class Downloader
 {
-
     public static function download()
     {
-
         $fileName = 'latest.zip';
-        $path     = dirname(__FILE__);;
+
+        $path = dirname(__FILE__);
 
         copy('https://wordpress.org/latest.zip', $fileName);
 
-
-        $zip = new ZipArchive();
+        $zip = new \ZipArchive();
 
         $res = $zip->open($fileName);
-        if ($res === TRUE) {
-            $zip->extractTo($path);
-            $zip->close();
-            echo 'woot!';
-
-            Downloader::rcopy('wordpress', $path, $path);
-
-            Downloader::rrmdir('wordpress');
-
-        } else {
+        if ($res === false) {
             echo 'doh!';
+            return;
         }
+        $zip->extractTo($path);
+        $zip->close();
+        echo 'woot!';
 
+        Downloader::rCopy('wordpress', $path, $path);
 
+        Downloader::rRmdir('wordpress');
     }
-
 
     // Function to remove folders and files
-    static function rrmdir($dir)
+    private static function rRmdir(string $dir)
     {
-        if (is_dir($dir)) {
-            $files = scandir($dir);
-            foreach ($files as $file)
-                if ($file != "." && $file != "..") Downloader::rrmdir("$dir/$file");
-            rmdir($dir);
-        } else if (file_exists($dir)) unlink($dir);
+        if (!is_dir($dir)) {
+            if (file_exists($dir)) {
+                unlink($dir);
+                return;
+            }
+        }
+        $files = scandir($dir);
+        foreach ($files as $file) {
+            if ($file !== "." && $file !== "..") {
+                Downloader::rRmdir("$dir/$file");
+            }
+        }
+        rmdir($dir);
     }
 
-// Function to Copy folders and files
-    static function rcopy($src, $dst, $skip)
+    // Function to Copy folders and files
+    private static function rCopy(string $src, string $dst, string $skip)
     {
-//    if (file_exists($dst))
-//        Downloader::rrmdir($dst);
-        if (is_dir($src)) {
-            if ($dst != $skip)
-                mkdir($dst);
-            $files = scandir($src);
-            foreach ($files as $file)
-                if ($file != "." && $file != "..")
-                    Downloader::rcopy("$src/$file", "$dst/$file", $skip);
-        } else if (file_exists($src))
-            copy($src, $dst);
+        if (!is_dir($src)) {
+            if (file_exists($src)) {
+                copy($src, $dst);
+            }
+            return;
+        }
+        if ($dst !== $skip) {
+            mkdir($dst);
+        }
+        $files = scandir($src);
+        foreach ($files as $file) {
+            if ($file !== "." && $file !== "..") {
+                Downloader::rCopy("$src/$file", "$dst/$file", $skip);
+            }
+        }
     }
-
 }
